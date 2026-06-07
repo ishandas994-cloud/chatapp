71f050d3c885af8a01b9f061777b9a4b2d40eab1
Ishan Das <ishandas994@gmail.com>
2026-06-07 20:53:06 +0530

    new codes

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -4,3 +4,16 @@ import { useAuth } from '../context/AuthContext';
 import { useSocket } from '../context/SocketContext';
 import CallUI from '../components/CallUI';
 import EmojiGifPicker from '../components/EmojiGifPicker';
+
+function Avatar({ user, size = 44, online = false }) {
+  const initials = user?.name?.split(' ').map(n => n[0]).join('').toUpperCase().slice(0,2) || '?';
+  return (
+    <div className="av-wrap" style={{ width: size, height: size }}>
+      {user?.avatar
+        ? <img className="av-img" src={user.avatar} alt={user.name} style={{ width: size, height: size }} />
+        : <div className="av-placeholder" style={{ width: size, height: size, fontSize: size * 0.36 }}>{initials}</div>
+      }
+      {online && <span className="online-dot" />}
+    </div>
+  );
+}
